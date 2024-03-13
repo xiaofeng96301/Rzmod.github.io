bb523977e224216e014646f12e04937c429b0300
枫枫子 <115228046+xiaofeng96301@users.noreply.github.com>
2024-03-13 21:46:42 +0800

    Add files via upload

--- json/index.js
+++ json/index.js
@@ -7,78 +7,37 @@ var navigatin = [{
 		[' 								     ', '	', '	  ', '	     ', '	   ', '	  '],
 		[' 								     ', '   ', '	  ', '	     ', '	   ', '	  '],
 		[' 								     ', ' 	', '	  ', '	     ', '	   ', '	  ']
-
 	]
 }];
-var versionpos=["2.0","1.8"]
+//name里面是导航条的内容“ ,'文字' ”这样往后添加导航条
+//names里面添加二级窗口导航（没什么用，因为导航栏根本没写）
+var versionpos = ["2.0", "1.8"]//版本号导航，没有此导航也没事，本来没啥用
 var version = {
-	"2.0": [{
-		"name": "人类",
-		"attribute": "不可被踩踏 不动时增强",
-		"image":["tan.jpg","tan.jpg"],
-		"content": "\
+	"2.0": [
+	{//从此括号复制，
+		"name": "人类",/*<-name为大标题 搜索和定位还有导航条全靠这里的名称，此处不允许出现特殊符号*/
+		"attribute": "不可被踩踏 不动时增强",/*<-attribute为特殊注释等类型的方式方法，此段在最后显示*/
+		"image": ["tan.jpg", "tan.jpg"],/*<-image为详情右侧图片栏位 按顺序添加为从上到下*/
+		"content":/*<-content 这段是中心介绍和详情文本 左斜杠“\”为回车 */"\ 
+		\
 		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
 		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
 		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
-		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
-		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
-		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababb\
-		abababaddadrhtjykabababaabababaddadrhtjykababababababababaddadrhtjykabababababbab\
 		"
-	}, {
+	},//从此括号复制结束
+	{//从此括号复制，
+		"name": "演示",/*<-name为大标题 搜索和定位还有导航条全靠这里的名称，此处不允许出现特殊符号*/
+		"attribute": "不可被踩踏 不动时增强",/*<-attribute为特殊注释等类型的方式方法，此段在最后显示*/
+		"image": ["czu.jpg", "tan.jpg"],/*<-image为详情右侧图片栏位 按顺序添加为从上到下*/
+		"content":/*<-content 这段是中心介绍和详情文本 左斜杠“\”为回车 */"\ 
+		\
+		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
+		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
+		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
+		"
+	},//从此括号复制结束
+	
+	{
 		"name": "陆军",
 		"attribute": "abbababa",
 		"content": "abababaddadrhtjykababababab"
@@ -114,7 +73,7 @@ var version = {
 	"1.8": [{
 		"name": "人类",
 		"attribute": "可被踩踏",
-		"image":["tan.jpg","jdn.jpg"],
+		"image": ["tan.jpg", "jdn.jpg"],
 		"content": "abmmmmmmmmmmmmmmmmmmmb"
 	}, {
 		"name": "海军",
