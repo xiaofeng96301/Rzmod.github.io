97feb5075cf348974d1450a17864b23c4730b9d9
枫枫子 <115228046+xiaofeng96301@users.noreply.github.com>
2024-03-13 21:45:12 +0800

    内容更新
    add

---
+++ index.js
@@ -0,0 +1,89 @@
+var navigatin = [{
+	name: ['<img src="images/Rz.png" class="icon" alt="图片加载错误">', '首页', '简介', '教程', 'Wiki', '关于'],
+	names: [
+		[' 								     ', '	', '	  ', '基础入门', '	   ', '	  '],
+		['	 								 ', '	', '	  ', '细节入门', '	   ', '	  '],
+		[' 								     ', '	', '	  ', '版本分类', '	   ', '	  '],
+		[' 								     ', '	', '	  ', '	     ', '	   ', '	  '],
+		[' 								     ', '   ', '	  ', '	     ', '	   ', '	  '],
+		[' 								     ', ' 	', '	  ', '	     ', '	   ', '	  ']
+	]
+}];
+//name里面是导航条的内容“ ,'文字' ”这样往后添加导航条
+//names里面添加二级窗口导航（没什么用，因为导航栏根本没写）
+var versionpos = ["2.0", "1.8"]//版本号导航，没有此导航也没事，本来没啥用
+var version = {
+	"2.0": [
+	{//从此括号复制，
+		"name": "人类",/*<-name为大标题 搜索和定位还有导航条全靠这里的名称，此处不允许出现特殊符号*/
+		"attribute": "不可被踩踏 不动时增强",/*<-attribute为特殊注释等类型的方式方法，此段在最后显示*/
+		"image": ["tan.jpg", "tan.jpg"],/*<-image为详情右侧图片栏位 按顺序添加为从上到下*/
+		"content":/*<-content 这段是中心介绍和详情文本 左斜杠“\”为回车 */"\ 
+		\
+		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
+		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
+		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
+		"
+	},//从此括号复制结束
+	{//从此括号复制，
+		"name": "演示",/*<-name为大标题 搜索和定位还有导航条全靠这里的名称，此处不允许出现特殊符号*/
+		"attribute": "不可被踩踏 不动时增强",/*<-attribute为特殊注释等类型的方式方法，此段在最后显示*/
+		"image": ["czu.jpg", "tan.jpg"],/*<-image为详情右侧图片栏位 按顺序添加为从上到下*/
+		"content":/*<-content 这段是中心介绍和详情文本 左斜杠“\”为回车 */"\ 
+		\
+		abababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababab\
+		abababaddadrhtjykababababaabababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykababababababababaddadrhtjykabababababb\
+		abababaddadrhtjykabababababababaddadrhtjykababababababababaddadrhtjykabababababab\
+		"
+	},//从此括号复制结束
+	
+	{
+		"name": "陆军",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "空军",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "海军",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "abab",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "dav",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "ajht",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "kuy",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "il",
+		"attribute": "abbababa",
+		"content": "abababaddadrhtjykababababab"
+	}],
+	"1.8": [{
+		"name": "人类",
+		"attribute": "可被踩踏",
+		"image": ["tan.jpg", "jdn.jpg"],
+		"content": "abmmmmmmmmmmmmmmmmmmmb"
+	}, {
+		"name": "海军",
+		"attribute": "abba阿达    baba",
+		"content": "abababaddadrhtjykababababab"
+	}, {
+		"name": "abab",
+		"attribute": "abbababa",
+		"content": "abababaadfgdztetwazweafae色通过色染颜色有黑色ddadrhtjykababababab"
+	}]
+
+
+};
